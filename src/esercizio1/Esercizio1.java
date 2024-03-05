6979b15fedad9ae0695580c7872ce6a3406eb567
MichelaVivacqua <vivacquamichela@gmail.com>
2024-03-05 14:30:40 +0100

    Initial commit

---
+++ src/esercizio1/Esercizio1.java
@@ -0,0 +1,13 @@
+package esercizio1;
+
+public class Esercizio1 {
+    public static void main(String[] args) {
+       System.out.printf("Hello World!");
+
+        String pariODispari = "trentatre";
+        if (pariODispari.length() % 2 == 0)
+        System.out.println("La stringa ha un numero pari di caratteri");
+        else System.out.println("La stringa ha un numero pari di caratteri");
+
+    }
+}
